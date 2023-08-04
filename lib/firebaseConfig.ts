66aeea4f30a964606920ab69cccb6820c8683279
KishiTheMechanic <s.kishi@elsoul.nl>
2023-08-05 00:29:54 +0200

    first commit

---
+++ lib/firebaseConfig.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  projectId: "skeet-solana",
+  appId: "1:970336459442:web:292c14b59159c5506407ab",
+  storageBucket: "skeet-solana.appspot.com",
+  locationId: "europe-west3",
+  apiKey: "AIzaSyBYXs9JEym42gJCuO1OT7mJ_tyJUpJRB84",
+  authDomain: "skeet-solana.firebaseapp.com",
+  messagingSenderId: "970336459442",
+  measurementId: "G-PR5LDPJSPY",
+}
+export default firebaseConfig;
