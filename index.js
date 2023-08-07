a368dfd3df42f9a2b46da6915defdba8c94ef4bb
KishiTheMechanic <s.kishi@elsoul.nl>
2023-08-08 01:12:32 +0200

    we can see the logs in console

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from 'expo'
 
-import App from './App';
+import App from './App'
+
+import { LogBox } from 'react-native'
+LogBox.ignoreLogs(['']) // Ignore log notification by message
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(App)
