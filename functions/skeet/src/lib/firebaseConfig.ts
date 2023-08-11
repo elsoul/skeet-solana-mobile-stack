703ab5c22ddedf2ce912604844588627b9b7e0e6
KishiTheMechanic <s.kishi@elsoul.nl>
2023-08-11 21:04:07 +0200

    skeet login

---
+++ functions/skeet/src/lib/firebaseConfig.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  projectId: 'skeet-solana-mobile-stack',
+  appId: '1:970336459442:web:292c14b59159c5506407ab',
+  storageBucket: 'skeet-solana-mobile-stack.appspot.com',
+  locationId: 'europe-west3',
+  apiKey: 'AIzaSyBYXs9JEym42gJCuO1OT7mJ_tyJUpJRB84',
+  authDomain: 'skeet-solana-mobile-stack.firebaseapp.com',
+  messagingSenderId: '970336459442',
+  measurementId: 'G-PR5LDPJSPY',
+}
+export default firebaseConfig
