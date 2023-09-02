c8f69eba57fb27794d08f822c3d13732bb4e32d9
KishiTheMechanic <s.kishi@elsoul.nl>
2023-09-02 23:56:17 +0200

    add ignore change firebase config

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -2,7 +2,6 @@ const firebaseConfig = {
   projectId: 'skeet-solana-mobile-stack',
   appId: '1:970336459442:web:292c14b59159c5506407ab',
   storageBucket: 'skeet-solana-mobile-stack.appspot.com',
-  locationId: 'europe-west3',
   apiKey: 'AIzaSyBYXs9JEym42gJCuO1OT7mJ_tyJUpJRB84',
   authDomain: 'skeet-solana-mobile-stack.firebaseapp.com',
   messagingSenderId: '970336459442',
